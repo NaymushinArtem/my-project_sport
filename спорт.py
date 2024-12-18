4bf43a3a487d84313fd87cc084752e5054497b69
Наймушин Артем <naymushinaa1@gmail.com>
2024-12-18 12:49:25 +0700

    Добавлен файл .py

---
+++ спорт.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Хранение зарегистрированных участников и соревнований
+participants = []
+competitions = []
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    participants.append(data)
+    return jsonify({"message": "Участник зарегистрирован", "participant": data}), 201
+
+@app.route('/competitions', methods=['POST'])
+def create_competition():
+    data = request.json
+    competitions.append(data)
+    return jsonify({"message": "Соревнование создано", "competition": data}), 201
+
+@app.route('/competitions', methods=['GET'])
+def get_competitions():
+    return jsonify(competitions), 200
+
+@app.route('/participants', methods=['GET'])
+def get_participants():
+    return jsonify(participants), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
